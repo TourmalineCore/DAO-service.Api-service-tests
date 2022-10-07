cebfba83946120d35a768bcd28d0797db2c4aa4b
Iliandr123 <ilia-scorpion@mail.ru>
2022-10-07 15:52:27 +0500

    refactor(cypress config): #4: remove comments and renamed script to kebab style

--- cypress.config.js
+++ cypress.config.js
@@ -1,42 +1,29 @@
 const { defineConfig } = require("cypress");
 const { exec } = require('child_process')
-const { isFileExist, findFiles } = require('cy-verify-downloads');
 
 module.exports = defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3000',
-  },
-  video: false,
-  screenshotOnRunFailure: false,
-  // retries: {
-  //   runMode: 1,
-  //   openMode: 0,
-  // },
-  requestTimeout: 30000,
 
-  // screenshotsFolder: 'cypress/fixtures/screenshots',
+    video: false,
 
-  // viewportWidth: 1280,
-  // viewportHeight: 720,
-  projectId: "shr3v9",
+    screenshotOnRunFailure: false,
 
-  e2e: {
+    requestTimeout: 30000,
+
+    projectId: "shr3v9",
+
     setupNodeEvents(on, config) {
-        on('before:browser:launch', (browser, launchOptions) => {
-          if (browser.name === 'chrome' && browser.isHeadless) {
-            // console.log('TRUE');
-            launchOptions.args.push('--window-size=4480,2520');
-            // console.log(launchOptions.args);
-            return launchOptions
-          }
-        })
-        on('after:screenshot', (details) => {
-          console.log(details)
-          exec('sh script2.sh')
-        })
-        on('task', { isFileExist, findFiles });
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--window-size=4480,2520');
+          return launchOptions
+        }
+      })
+      on('after:screenshot', (details) => {
+        exec('sh move-screenshot.sh');
+      })
     },
-  },
+  }
 });
